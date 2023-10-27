9cbc9f397e9f22c3ffaf97b1d3528a672ed2f8fb
Noëlle Anthony <noelle@noelle.codes>
2023-10-27 18:40:06 -0400

    Add pre-commit

--- bussard.py
+++ bussard.py
@@ -2,6 +2,9 @@ import feedparser
 import opyml
 import psycopg2
 
+
 def read_archive(self, filename):
+    # filename is assumed to be relative to the directory you're calling this
+    # from, or globally qualified. It cannot be a URL or non-local file.
     with open(filename, "r") as file:
         contents = file.read(filename)
